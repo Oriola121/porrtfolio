c35212127d8515305079b41b9df6d45dcaf652b4
Muhibudeen AbdurRahman Oriola <muhibudeen.oriola@gmail.com>
2024-04-19 21:15:46 +0100

    gold

--- src/Rite2.js
+++ src/Rite2.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import { IoMdDownload } from 'react-icons/io'
+import ProgressBar from "@ramonak/react-progress-bar"
 
 function Rite2() {
   return (
     <R2cont>
         <R2wrap>
           <div className='skilrate'><p>Front-End</p>
-            <p>Back-End</p>
-            <p>Graphics</p></div>
-            <div className='ceevee'><a href='https://goldceevee.tiiny.site' target='_blank' className='clink'>
-              <p>Download CV</p>
-              <IoMdDownload/></a>
-            </div>
+            <ProgressBar completed={80} customLabel="80%" width='202px' height='10px' labelSize='7.5px'/>
+          </div>
+          <div className='skilrate'><p>Back-End</p>
+            <ProgressBar completed={20} customLabel="20%" width='205px' height='10px' labelSize='7.5px'/>
+          </div>
+          <div className='skilrate'><p>Graphics</p>
+          <ProgressBar completed={85} customLabel="85%" width='210px' height='10px' labelSize='7.5px'/>
+          </div>
+          <a href='https://goldceevee.tiiny.site' target='_blank' className='clink'>
+            <p>Download CV</p>
+            <IoMdDownload/></a>
         </R2wrap>
     </R2cont>
   )
@@ -23,28 +29,27 @@ const R2cont = styled.div`
 width: 100%;
 height: 100%;
 display: flex;
+justify-content: center;
 margin-top: 10px;
 `
 const R2wrap = styled.div`
-width: 100%;
+width: 90%;
 height: 100%;
-font-family: times new romans;
+display: flex;
+flex-direction: column;
 
 .skilrate{
   display: flex;
-  justify-content: space-around;
-}
-
-.ceevee{
-  margin: 10px 20px;
-  display: flex;
-  justify-content: center;
-  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  margin-top: 10px;
 }
 
 .clink{
+  margin: 10px 20px;
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 3px;
 }
 
